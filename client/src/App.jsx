151c75decb23ae1523413a66cfb543001059fc08
kachamachkov <kachamachkovboyan@gmail.com>
2024-08-05 16:58:18 +0300

    Add router, render 3 jobs at home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,9 @@ import Hero from './components/Hero';
 import HomeCards from './components/HomeCards';
 import JobListings from './components/JobListings';
 
+import { Route, Routes } from 'react-router-dom';
+import ViewAllJobs from './components/ViewAllJobs';
+
 function App() {
   return (
     <>
@@ -10,16 +13,7 @@ function App() {
       <Hero />
       <HomeCards />
       <JobListings />
-
-
-      <section className="m-auto max-w-lg my-10 px-6">
-        <a
-          href="jobs.html"
-          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >
-          View All Jobs
-        </a>
-      </section>
+      <ViewAllJobs />
     </>
   );
 }
