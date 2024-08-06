90ffb17dd77da99ba646ea5d9c140ccefaed8195
kachamachkov <kachamachkovboyan@gmail.com>
2024-08-06 10:20:38 +0300

    Fix links across files, add 404 page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import {
 import HomePage from './pages/HomePage';
 import MainLayout from './layouts/MainLayout';
 import JobsPage from './pages/JobsPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 
 const router = createBrowserRouter(
@@ -17,6 +18,7 @@ const router = createBrowserRouter(
     <Route path='/' element={<MainLayout />} >
       <Route index element={<HomePage />} />
       <Route path='/jobs' element={<JobsPage />} />
+      <Route path='*' element={<NotFoundPage />} />
     </Route>)
 );
 
