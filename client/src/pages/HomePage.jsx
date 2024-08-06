6edc7233c7bc974407cf242979a131e90e4a39d1
kachamachkov <kachamachkovboyan@gmail.com>
2024-08-06 09:45:51 +0300

    Render all components into Home page

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,8 +1,16 @@
 import Hero from "../components/Hero";
+import HomeCards from "../components/HomeCards";
+import JobListings from "../components/JobListings";
+import ViewAllJobs from "../components/ViewAllJobs";
 
 const HomePage = () => {
   return (
-    <Hero />
+    <>
+      <Hero />
+      <HomeCards />
+      <JobListings />
+      <ViewAllJobs />
+    </>
   );
 };
 export default HomePage;
