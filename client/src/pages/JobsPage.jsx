f633ea67b5f528ac3233bdd34476d5a8bc0e1fd1
kachamachkov <kachamachkovboyan@gmail.com>
2024-08-06 10:35:50 +0300

    Render recent jobs at home and all jobs at jobs page

--- client/src/pages/JobsPage.jsx
+++ client/src/pages/JobsPage.jsx
@@ -1,6 +1,13 @@
+import JobListings from "../components/JobListings";
+
+
 const JobsPage = () => {
   return (
-    <div>JobsPage</div>
-  )
-}
-export default JobsPage
+    <section className="bg-blue-50 px-4 py-6">
+      <JobListings />
+    </section>
+  );
+};
+
+
+export default JobsPage;
