be45f704bb7b4f98e03ad209154fd060170af113
kachamachkov <kachamachkovboyan@gmail.com>
2024-08-06 09:54:47 +0300

    Added links to navigation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,13 +9,14 @@ import {
 
 import HomePage from './pages/HomePage';
 import MainLayout from './layouts/MainLayout';
+import JobsPage from './pages/JobsPage';
 
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout />} >
-
       <Route index element={<HomePage />} />
+      <Route path='/jobs' element={<JobsPage />} />
     </Route>)
 );
 
