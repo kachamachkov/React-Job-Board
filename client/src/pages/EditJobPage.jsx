6b88ca45f352364b146bbd09a4d287a01b75cd42
kachamachkov <kachamachkovboyan@gmail.com>
2024-08-07 22:42:36 +0300

    Render Edit Page

--- client/src/pages/EditJobPage.jsx
+++ client/src/pages/EditJobPage.jsx
@@ -1,57 +1,37 @@
-import { useState, useEffect } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useForm } from "../hooks/useForm";
+
+// const initialValues = {
+//   type
+//   title
+//   description
+//   salary
+//   location
+//   company
+//   company_description
+//   contact_email
+// }
+
+const initialValues = {
+  title: '',
+  type: 'Full-Time',
+  description: '',
+  location: '',
+  salary: 'Employer did not specify',
+  companyName: '',
+  companyDescription: '',
+  contactEmail: '',
+};
 
 
-const EditJobPage = ({ updateJobSubmit }) => {
-  const navigate = useNavigate();
-  const { id } = useParams();
-  const [job, setJob] = useState({});
+const EditJobPage = () => {
 
-  const [title, setTitle] = useState(job.title);
-  const [type, setType] = useState(job.type);
-  const [location, setLocation] = useState(job.location);
-  const [description, setDescription] = useState(job.description);
-  const [salary, setSalary] = useState(job.salary);
-  const [companyName, setCompanyName] = useState(job.company.name);
-  const [companyDescription, setCompanyDescription] = useState(job.company.description);
-  const [contactEmail, setContactEmail] = useState(job.company.contactEmail);
-
-  useEffect(() => {
-    (async () => {
-      try {
-        const res = await fetch(`http://localhost:3030/data/jobs/${id}`);
-        const data = await res.json();
-        setJob(data);
-
-      } catch (err) {
-        console.log(err.message);
-      } finally {
-      }
-    })();
-  }, []);
-
-  console.log(job)
-
-  const submitForm = (e) => {
-    e.preventDefault();
-
-    const updatedJob = {
-      _id,
-      title,
-      type,
-      description,
-      location,
-      salary,
-      company: {
-        name: companyName,
-        description: companyDescription,
-        contactEmail
-      }
-    };
-
-    updateJobSubmit(updatedJob);
-    return navigate(`/jobs/${_id}`);
-  };
+  const {
+    values,
+    changeHandler,
+    submitHandler
+  } = useForm(initialValues, (values) => {
+    console.log(values);
+  });
 
   return (
     <section className="bg-indigo-50">
@@ -59,7 +39,7 @@ const EditJobPage = ({ updateJobSubmit }) => {
         <div
           className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0"
         >
-          <form onSubmit={submitForm}>
+          <form onSubmit={submitHandler}>
             <h2 className="text-3xl text-center font-semibold mb-6">Add Job</h2>
 
             <div className="mb-4">
@@ -71,8 +51,8 @@ const EditJobPage = ({ updateJobSubmit }) => {
                 name="type"
                 className="border rounded w-full py-2 px-3"
                 required
-                value={type}
-                onChange={(e) => setType(e.target.value)}
+                value={values.type}
+                onChange={changeHandler}
               >
                 <option value="Full-Time">Full-Time</option>
                 <option value="Part-Time">Part-Time</option>
@@ -92,8 +72,8 @@ const EditJobPage = ({ updateJobSubmit }) => {
                 className="border rounded w-full py-2 px-3 mb-2"
                 placeholder="eg. Beautiful Apartment In Miami"
                 required
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
+                value={values.title}
+                onChange={changeHandler}
               />
             </div>
             <div className="mb-4">
@@ -108,8 +88,8 @@ const EditJobPage = ({ updateJobSubmit }) => {
                 className="border rounded w-full py-2 px-3"
                 rows="4"
                 placeholder="Add any job duties, expectations, requirements, etc"
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
+                value={values.description}
+                onChange={changeHandler}
               ></textarea>
             </div>
 
@@ -122,8 +102,8 @@ const EditJobPage = ({ updateJobSubmit }) => {
                 name="salary"
                 className="border rounded w-full py-2 px-3"
                 required
-                value={salary}
-                onChange={(e) => setSalary(e.target.value)}
+                value={values.salary}
+                onChange={changeHandler}
               >
                 <option value="Employer did not specify">Do not specify</option>
                 <option value="1.5-2.5K BGN">1.5-2.5K BGN</option>
@@ -145,8 +125,8 @@ const EditJobPage = ({ updateJobSubmit }) => {
                 className='border rounded w-full py-2 px-3 mb-2'
                 placeholder='Company Location'
                 required
-                value={location}
-                onChange={(e) => setLocation(e.target.value)}
+                value={values.location}
+                onChange={changeHandler}
               />
             </div>
 
@@ -162,8 +142,8 @@ const EditJobPage = ({ updateJobSubmit }) => {
                 name="company"
                 className="border rounded w-full py-2 px-3"
                 placeholder="Company Name"
-                value={companyName}
-                onChange={(e) => setCompanyName(e.target.value)}
+                value={values.companyName}
+                onChange={changeHandler}
               />
             </div>
 
@@ -179,8 +159,8 @@ const EditJobPage = ({ updateJobSubmit }) => {
                 className="border rounded w-full py-2 px-3"
                 rows="4"
                 placeholder="What does your company do?"
-                value={companyDescription}
-                onChange={(e) => setCompanyDescription(e.target.value)}
+                value={values.companyDescription}
+                onChange={changeHandler}
               ></textarea>
             </div>
 
@@ -197,8 +177,8 @@ const EditJobPage = ({ updateJobSubmit }) => {
                 className="border rounded w-full py-2 px-3"
                 placeholder="Email address for applicants"
                 required
-                value={contactEmail}
-                onChange={(e) => setContactEmail(e.target.value)}
+                value={values.contactEmail}
+                onChange={changeHandler}
               />
             </div>
             <div>
--- client/src/pages/JobPage.jsx
+++ client/src/pages/JobPage.jsx
@@ -51,7 +51,7 @@ const JobPage = () => {
 
       await jobsAPI.remove(jobId);
 
-      navigate('/')
+      navigate('/');
 
     } catch (err) {
       console.log(err.message);
@@ -112,16 +112,13 @@ const JobPage = () => {
                 </h3>
 
                 <div className="mb-4">
-                  {/* render comments here */}
                   {comments.map(comment => (
                     <p key={comment._id}>{comment.author.email}: {comment.text}</p>
 
                   ))}
                 </div>
 
-                {/* <h3 className="text-indigo-800 text-lg font-bold mb-2">Salary</h3>
 
-                <p className="mb-4">{job.salary}</p> */}
               </div>
 
               {isAuthenticated && (
@@ -176,7 +173,7 @@ const JobPage = () => {
                 <div className="bg-white p-6 rounded-lg shadow-md mt-6">
                   <h3 className="text-xl font-bold mb-6">Manage Job</h3>
                   <Link
-                    to={`/jobs/edit/${job.id}`}
+                    to={`/jobs/edit/${jobId}`}
                     className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                   >Edit Job
                   </Link>
